a2f6885d956141f73769e573bc6f3fc2f27ece6c
Seoin <tjdls111@naver.com>
2022-03-09 15:39:14 +0900

    vanilla js + redux로 counter 만들기 refactor

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,15 +4,22 @@ const addBtn = document.querySelector("#addBtn");
 const minusBtn = document.querySelector("#minusBtn");
 const number = document.querySelector("#number");
 
+// 변수를 사용하면, 실수를 줄일 수 있음
+const add = "add";
+const minus = "minus";
+
 // 리덕스- 데이터 한 곳에 모이게 함.
 // 이 함수만 데이터를 바꿀 수 있음.
+// state를 리턴.
 const countModifier = (count = 0, action) => {
-  if (action.type === "add") {
-    return count + 1;
-  } else if (action.type === "minus") {
-    return count - 1;
-  } else {
-    return count;
+  // if-else if -else보다 switch문이 복 편함
+  switch (action.type) {
+    case add:
+      return count + 1;
+    case minus:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
@@ -22,9 +29,10 @@ const onChange = () => {
 };
 countStore.subscribe(onChange);
 addBtn.addEventListener("click", () => {
-  countStore.dispatch({ type: "add" });
+  // action은 오브젝트여야 하고, type이 필수로 있어야 함.
+  countStore.dispatch({ type: add });
 });
 
 minusBtn.addEventListener("click", () => {
-  countStore.dispatch({ type: "minus" });
+  countStore.dispatch({ type: minus });
 });
