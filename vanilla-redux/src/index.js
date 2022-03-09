d1c8ca11b6434df61fa3cd9fc3cf1293a5505291
Seoin <tjdls111@naver.com>
2022-03-09 15:32:39 +0900

    vanilla javascript + redux로 counter

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,23 +1,30 @@
+import { createStore } from "redux";
+
 const addBtn = document.querySelector("#addBtn");
 const minusBtn = document.querySelector("#minusBtn");
 const number = document.querySelector("#number");
 
-let counter = 0;
-number.innerHTML = counter;
-
-const updateText = () => {
-  number.innerHTML = counter;
+// 리덕스- 데이터 한 곳에 모이게 함.
+// 이 함수만 데이터를 바꿀 수 있음.
+const countModifier = (count = 0, action) => {
+  if (action.type === "add") {
+    return count + 1;
+  } else if (action.type === "minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-  counter += 1;
-  updateText();
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
+countStore.subscribe(onChange);
+addBtn.addEventListener("click", () => {
+  countStore.dispatch({ type: "add" });
+});
 
-const handleMinus = () => {
-  counter -= 1;
-  updateText();
-};
-
-addBtn.addEventListener("click", handleAdd);
-minusBtn.addEventListener("click", handleMinus);
+minusBtn.addEventListener("click", () => {
+  countStore.dispatch({ type: "minus" });
+});
